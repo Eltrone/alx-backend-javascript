a6cd564e2bda743c1441600fe8481a1cd696121d
Eltrone <amineelorche@gmail.com>
2024-06-26 22:10:48 +0000

    script added

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,11 @@
+// Define the default export of this module
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => [resolve, reject]);
+  // Returns a new Promise object
+  return new Promise((resolve, reject) => {
+    // The executor function of the Promise, which has access to
+    // the `resolve` and `reject` functions. This setup is a bit unusual
+    // because it simply returns an array containing `resolve` and `reject`
+    // functions instead of invoking them based on some condition.
+    [resolve, reject];
+  });
 }
