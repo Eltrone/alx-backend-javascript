2a0955a092183b829ef3f0efb61889de0fb5cb41
Eltrone <amineelorche@gmail.com>
2024-06-26 22:44:53 +0000

    script added

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,15 +1,9 @@
-// 1-promise.js
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      // Si le paramètre `success` est vrai, résolvez la promesse avec un objet spécifique
-      resolve({
-        status: 200,
-        body: 'Success'
-      });
+      resolve({ status: 200, body: 'Success' });
     } else {
-      // Si le paramètre `success` est faux, rejetez la promesse avec un message d'erreur
-      reject(new Error("The fake API is not working currently"));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
