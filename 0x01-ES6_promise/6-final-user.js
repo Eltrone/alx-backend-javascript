fdec2c1ca7d705476b5596b52d43d6d44c72b3ff
Eltrone <amineelorche@gmail.com>
2024-06-26 23:06:42 +0000

    script added

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,25 +3,20 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const res = [];
+
   try {
     const user = await signUpUser(firstName, lastName);
     res.push({ status: 'fulfilled', value: user });
   } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: `Error: ${err.message}`,
-    });
+    res.push({ status: 'rejected', value: err.message });
   }
 
   try {
     await uploadPhoto(fileName);
     res.push({ status: 'fulfilled', value: `${fileName} processed successfully` });
   } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: `Error: ${fileName} cannot be processed`,
-    });
+    res.push({ status: 'rejected', value: err.message });
   }
-  
+
   return res;
 }
