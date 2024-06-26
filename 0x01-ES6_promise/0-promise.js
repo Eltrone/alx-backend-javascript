76521a590824ecb02fe90d4f9ed494f0cfa7ba1c
Eltrone <amineelorche@gmail.com>
2024-06-26 22:35:00 +0000

    script added

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,3 @@
-// Exporte une fonction qui retourne une promesse résolue.
 export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    resolve("Opération réussie");
-  });
+  return new Promise((resolve, reject) => [resolve, reject]);
 }
