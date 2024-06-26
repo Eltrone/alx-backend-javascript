496e19be7528b17d6e1983fec5caef26cecda717
Eltrone <amineelorche@gmail.com>
2024-06-26 22:30:42 +0000

    script added

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,6 @@
-// Exporte une fonction qui retourne une promesse.
+// Exporte une fonction qui retourne une promesse résolue.
 export default function getResponseFromAPI() {
-  // Crée et retourne une nouvelle Promesse.
-  return new Promise((resolve, reject) => {
-    // Simuler un comportement asynchrone, résout la promesse avec un message.
-    resolve("Operation réussie");
-
-    // Pour simuler une erreur, vous pouvez utiliser `reject` comme ceci :
-    // reject(new Error("Échec de l'opération"));
+  return new Promise((resolve) => {
+    resolve("Opération réussie");
   });
 }
