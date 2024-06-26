99a1ed07fedf72dffba270c7f8ce871cac76d0d5
Eltrone <amineelorche@gmail.com>
2024-06-26 13:55:48 +0000

    file added

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,11 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulez une opération asynchrone, par exemple une demande à une API
-        setTimeout(() => {
-            resolve("Data received");
-        }, 1000);
-    });
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Supposons que cette opération soit asynchrone
+    setTimeout(() => {
+      // Ici, nous appelons `resolve` avec des données ou `reject` avec une erreur
+      resolve("Data received");
+      // ou, en cas d'erreur
+      // reject(new Error("Failed to get data"));
+    }, 1000);
+  });
 }
-
-export default getResponseFromAPI;
