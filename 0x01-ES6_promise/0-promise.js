8bcc2b61a0df326ec13e2fa3078b5ccb8e6d795e
Eltrone <amineelorche@gmail.com>
2024-06-26 22:52:55 +0000

    script added

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // La logique pour résoudre ou rejeter peut être ajoutée ici si nécessaire
-  });
+  return new Promise((resolve, reject) => {});
 }
