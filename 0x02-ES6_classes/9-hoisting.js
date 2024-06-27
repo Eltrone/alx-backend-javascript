7cd4a1b6ec88d7027781a2f68b4c5d4b3656a4e4
Eltrone <amineelorche@gmail.com>
2024-06-27 20:33:50 +0000

    file added

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,3 +1,4 @@
+// Définition de la classe HolbertonClass avant son utilisation
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -13,6 +14,11 @@ export class HolbertonClass {
   }
 }
 
+// Création des instances de HolbertonClass
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+// Définition de la classe StudentHolberton
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -24,14 +30,16 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName}`;
   }
 
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
+// Création des instances de StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
