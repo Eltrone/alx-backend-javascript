7450ac7265faccac72930266685723e34306671b
Eltrone <amineelorche@gmail.com>
2024-06-27 19:00:46 +0000

    file added

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,28 @@
-export default class Building {
-  constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
-    }
-    if (this.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
-    this._sqft = sqft;
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Appel du constructeur de la classe parente Building avec le paramètre sqft
+    this._floors = floors;
   }
 
   get sqft() {
     return this._sqft;
   }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
+
+// 6-main.js
+import SkyHighBuilding from './6-sky_high.js';
+
+const building = new SkyHighBuilding(140, 60);
+console.log(building.sqft); // Affiche 140
+console.log(building.floors); // Affiche 60
+console.log(building.evacuationWarningMessage()); // Affiche "Evacuate slowly the 60 floors"
