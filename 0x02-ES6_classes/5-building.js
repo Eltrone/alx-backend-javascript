d3c914382c31703b8bda6bf9e9a637da1a0c7793
Eltrone <amineelorche@gmail.com>
2024-06-27 18:52:05 +0000

    file added

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new TypeError('Cannot construct Building instances directly');
+    }
+    if (this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
