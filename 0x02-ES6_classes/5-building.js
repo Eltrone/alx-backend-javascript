84147779594eebb57c3b81d4d07afbb4d3374c32
Eltrone <amineelorche@gmail.com>
2024-06-27 20:14:11 +0000

    file added

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,28 +1,9 @@
-import Building from './5-building.js';
-
-export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
-    this._floors = floors;
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  get floors() {
-    return this._floors;
-  }
-
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
-  }
 }
-
-// 6-main.js
-import SkyHighBuilding from './6-sky_high.js';
-
-const building = new SkyHighBuilding(140, 60);
-console.log(building.sqft);
-console.log(building.floors);
-console.log(building.evacuationWarningMessage());
