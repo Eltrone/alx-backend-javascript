ac9033e0e31d2c16d7f652c30fc9314dba1acfe9
Eltrone <amineelorche@gmail.com>
2024-08-20 15:52:58 +0000

    file added

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,23 +1,19 @@
-// Importation de la bibliothèque readline pour faciliter la lecture de l'entrée standard
+// Utilisation du module readline pour gérer l'entrée/sortie standard.
 const readline = require('readline');
 
-// Création d'une interface readline pour gérer l'entrée/sortie standard
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// Affichage du message initial
+// Affiche le message de bienvenue.
 console.log('Welcome to Holberton School, what is your name?');
 
-// Attente de l'entrée de l'utilisateur
+// Gestion de l'entrée de l'utilisateur.
 rl.on('line', (input) => {
   console.log(`Your name is: ${input}`);
-  rl.close(); // Fermeture de l'interface après l'entrée de l'utilisateur
-});
-
-// Gestionnaire pour l'événement 'close' de readline
-rl.on('close', () => {
+  rl.close(); // Ferme l'interface de lecture après avoir reçu l'entrée.
+}).on('close', () => {
   console.log('This important software is now closing');
-  process.exit(0); // Sortie du programme
+  process.exit(0); // Assure que le programme se termine proprement.
 });
