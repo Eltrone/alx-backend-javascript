e284086be8af38d6a4dc65c5e075d22c3be7de33
Eltrone <amineelorche@gmail.com>
2024-08-20 15:58:00 +0000

    file added

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -12,8 +12,14 @@ console.log('Welcome to Holberton School, what is your name?');
 // Gestion de l'entrée de l'utilisateur.
 rl.on('line', (input) => {
   console.log(`Your name is: ${input}`);
-  rl.close(); // Ferme l'interface de lecture après avoir reçu l'entrée.
-}).on('close', () => {
+  // Vérifie si l'entrée vient d'un terminal ou d'un pipe.
+  if (rl.isTTY) {
+    rl.close(); // Ferme l'interface de lecture après avoir reçu l'entrée si c'est interactif.
+  }
+});
+
+// Événement 'close' déclenché par rl.close().
+rl.on('close', () => {
   console.log('This important software is now closing');
   process.exit(0); // Assure que le programme se termine proprement.
 });
