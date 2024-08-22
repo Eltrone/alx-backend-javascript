8785352965de8d094afcc24f781a71f2a9cf3248
Eltrone <amineelorche@gmail.com>
2024-08-22 16:07:35 +0000

    file added

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.listen(7865, () => {
     console.log('API available on localhost port 7865');
+});
 
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,55 +1,30 @@
+//api.test.js
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
-const server = require('./api'); // Ensure this is properly set up to import your Express app
 
 chai.use(chaiHttp);
 
-describe('API Endpoint Testing', () => {
-    after(() => {
-      server.close(); // Properly close the server after tests
-    });
+describe('API server', () => {
+  it('should respond with status code 200 for GET /cart/12 (valid ID)', (done) => {
+    chai.request('http://localhost:7865')
+      .get('/cart/12')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.text).to.contain('Payment methods for cart 12');
+        done();
+      });
+  });
 
-    it('should successfully respond with HTTP status 200 when accessing GET /', (done) => {
-        chai.request(server)
-            .get('/')
-            .end((error, response) => {
-                expect(error).to.be.null;
-                expect(response).to.have.status(200);
-                done();
-            });
-    });
-
-    it('should return the message "Welcome to the payment system" on GET /', (done) => {
-        chai.request(server)
-            .get('/')
-            .end((error, response) => {
-                expect(error).to.be.null;
-                expect(response.text).to.equal('Welcome to the payment system');
-                done();
-            });
-    });
-
-    // Test suite for new cart endpoint
-    describe('Cart Endpoint', () => {
-        it('should return "Payment methods for cart 12" when id is a number', (done) => {
-            chai.request(server)
-                .get('/cart/12')
-                .end((error, response) => {
-                    expect(error).to.be.null;
-                    expect(response.text).to.equal('Payment methods for cart 12');
-                    expect(response).to.have.status(200);
-                    done();
-                });
-        });
-
-        it('should return a 404 status code when id is NOT a number', (done) => {
-            chai.request(server)
-                .get('/cart/hello')
-                .end((error, response) => {
-                    expect(response).to.have.status(404);
-                    done();
-                });
-        });
-    });
+  it('should respond with status code 404 for GET /cart/hello (invalid ID)', (done) => {
+    chai.request('http://localhost:7865')
+      .get('/cart/hello')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(404);
+        expect(res.text).to.equal('Invalid cart ID');
+        done();
+      });
+  });
 });
